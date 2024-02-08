6370d449d3e3420bebeac6e604df08a1a8493eaf
nabeel-ncz <nabeelnabhan115@gmail.com>
2024-02-08 11:18:05 +0530

    fix: otp attached to user-entity

--- auth/src/application/useCases/createUserUseCase.ts
+++ auth/src/application/useCases/createUserUseCase.ts
@@ -1,4 +1,6 @@
 import { IDependencies } from "@/application/interfaces/IDependencies";
+import { UserEntity } from "@/domain/entities";
+import { generateVerificationOTP } from "@/_lib/utils/otp";
 
 export const createUserUseCase = (dependencies: IDependencies) => {
     const {
@@ -6,8 +8,15 @@ export const createUserUseCase = (dependencies: IDependencies) => {
     } = dependencies;
 
     return {
-        execute: async (data: any) => {
-            return await create(data);
+        execute: async (data: UserEntity) => {
+            try {
+
+                data['otp'] = generateVerificationOTP();
+                return await create(data);
+                
+            } catch (error: any) {
+                throw new Error(error.message || "User creation failed");
+            }
         }
     }
 }
