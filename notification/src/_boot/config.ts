134ff5f6107add8d51afbb96210030b794309194
nabeel-ncz <nabeelnabhan115@gmail.com>
2024-02-09 17:19:05 +0530

    fix: sendgrid new account setup

--- notification/src/_boot/config.ts
+++ notification/src/_boot/config.ts
@@ -3,7 +3,7 @@ import { envString, envNumber } from "@/_boot/environment";
 export const config = {
     http: {
         host: envString('HOST', 'localhost'),
-        port: envNumber('PORT', 3002)
+        port: envNumber('PORT', 3003)
     },
     // mongo: {
     //     database: envString('DB_NAME', 'zakaa'),
@@ -17,7 +17,7 @@ export const config = {
     // },
     sendgrid: {
         api_key: envString('SENDGRID_API_KEY', 'testkey'),
-        email: envString('SENDGRID_TO_MAIL', 'testmail')
+        email: envString('SENDGRID_FROM_MAIL', 'testmail')
     },
     kafka: {
         broker_urls: envString('KAFKA_BROKER_URLS', 'localhost:29092'),
