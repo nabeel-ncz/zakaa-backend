182b18004d939a17264e2b8da2cb2bfc22db2bdb
nabeel-ncz <nabeelnabhan115@gmail.com>
2024-02-09 14:58:37 +0530

    fix: issue with logging

--- gateway-dev/src/_lib/logging/index.ts
+++ gateway-dev/src/_lib/logging/index.ts
@@ -1,17 +1,12 @@
-import express, {
+import {
     Application,
     Request,
     Response,
     NextFunction
 } from "express";
-import cookieParser from "cookie-parser";
 import morgan from "morgan";
 
 export const setupLogging = (app: Application) => {
-    
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    app.use(cookieParser());
 
     app.use(morgan('dev'));
     app.use(
