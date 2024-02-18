8e32bd34e8f49400d02e90fae1a67b880b5d3c01
nabeel-ncz <nabeelnabhan115@gmail.com>
2024-02-18 21:56:08 +0530

    feat: updated main file for service images to client

--- course/src/presentation/application.ts
+++ course/src/presentation/application.ts
@@ -4,6 +4,7 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import { dependencies } from "@/_boot/dependencies";
 import { courseRoutes } from "@/presentation/routes/courseRoutes";
+import path from "path";
 
 const app: Application = express();
 
@@ -11,6 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(helmet());
+
+app.use("/api/course/images", express.static(path.join(__dirname, "..", "public", "images")));
 
 app.get('/', (req: Request, res: Response) => {
     res.status(200).json({
