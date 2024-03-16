b1cbfff2987b01d667b5c8c2553a2822f32be168
nabeel-ncz <nabeelnabhan115@gmail.com>
2024-03-16 10:02:46 +0530

    feat: issue with enrollment progress

--- course/src/infrastructure/database/mongo/repositories/enrollment/updateEnrollment.ts
+++ course/src/infrastructure/database/mongo/repositories/enrollment/updateEnrollment.ts
@@ -2,7 +2,14 @@ import { Enrollment } from "@/infrastructure/database/mongo/models";
 import { EnrollmentEntity } from "@/domain/entities";
 
 export const updateEnrollment = async (
-    { enrollmentId, lessonId, totalTimeWatched }: any
+    { enrollmentId, lessonId, totalTimeWatched = 0, status, completedLesson, completedAssessment }: {
+        enrollmentId: string;
+        lessonId: string;
+        totalTimeWatched: number;
+        status: 'lesson-completed' | 'assessment-completed' | 'none';
+        completedLesson?: string;
+        completedAssessment?: string;
+    }
 ): Promise<EnrollmentEntity | null> => {
     try {
 
@@ -12,20 +19,36 @@ export const updateEnrollment = async (
             throw new Error("Course enrollment failed!");
         }
 
-        if (!enrollment.progress) {
-            enrollment.progress = {};
+        if (status !== 'assessment-completed') {
+            if (!enrollment.progress) {
+                enrollment.progress = {};
+            }
+
+            if (!enrollment.progress.lessonProgress) {
+                enrollment.progress.lessonProgress = [];
+            }
+
+            const lessonIndex = enrollment?.progress?.lessonProgress?.findIndex((progress) => progress.lessonId.toString() === lessonId);
+
+            if (lessonIndex === -1) {
+                enrollment?.progress?.lessonProgress?.push({ lessonId, totalTimeWatched });
+            } else {
+                enrollment.progress.lessonProgress[lessonIndex].totalTimeWatched = totalTimeWatched;
+            }
         }
 
-        if (!enrollment.progress.lessonProgress) {
-            enrollment.progress.lessonProgress = [];
+        if (status === 'lesson-completed' && completedLesson) {
+            let exist = enrollment?.progress?.completedLessons?.findIndex((item) => item.toString() === completedLesson);
+            if (exist === -1) {
+                (enrollment?.progress?.completedLessons as string[])?.push(completedLesson);
+            }
         }
 
-        const lessonIndex = enrollment?.progress?.lessonProgress?.findIndex((progress) => progress.lessonId.toString() === lessonId);
-
-        if (lessonIndex === -1) {
-            enrollment?.progress?.lessonProgress?.push({ lessonId, totalTimeWatched });
-        } else {
-            enrollment.progress.lessonProgress[lessonIndex].totalTimeWatched = totalTimeWatched;
+        if (status === "assessment-completed" && completedAssessment) {
+            let exist = enrollment?.progress?.completedAssessments?.findIndex((item) => item.toString() === completedAssessment);
+            if (exist === -1) {
+                (enrollment?.progress?.completedAssessments as string[])?.push(completedAssessment);
+            }
         }
 
         const newEnr = await enrollment.save();
